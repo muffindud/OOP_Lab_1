ff1d1fe8e6418a3cd9186cc5e0c01572da870cdd
muffindud <ccatlabuga2002@gmail.com>
2022-09-17 17:12:41 +0300

    implement controls and game interface

--- main.c
+++ main.c
@@ -11,8 +11,14 @@
 
 FILE *saveFile, *saveState;
 const int initialState[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
+int currentState[4][4];
 int headIndex[2];
 //todo: add move history variable here
+
+// // BECAUSE WINDOWS F**KING SUCKS!
+// #ifdef _WIN32
+//     system("CHCP 65001");
+// #endif
 
 void clrscr(){
     #ifdef _WIN32
@@ -26,19 +32,50 @@ void clrscr(){
 void undo(){
     // Undoes the move
     // (need move history)
-    clrscr();
 
+    game();
 }
 
 void newGame(){
     // Creates a new game
-    clrscr();
     headIndex[0] = 3;
     headIndex[1] = 3;
-    int currentState[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
+    for(int j = 0; j < 4; j++){
+        for(int i = 0; i < 4; i++){
+            currentState[i][j] = 4 * j + i + 1;
+        }
+    }
+    currentState[3][3] = 0;
     int seed = time(NULL);
     for(int i = 0; i < 100; i++){
         //todo: pattern randomiser here
+    }
+    game();
+}
+
+void makeMove(char dir){
+    // Moves the headIndex
+    switch (dir){
+    case 'u':
+        currentState[headIndex[1]][headIndex[0]] = currentState[headIndex[1]][headIndex[0] + 1];
+        currentState[headIndex[1]][headIndex[0] + 1] = 0;
+        headIndex[0]++;
+        break;
+    case 'r':
+        currentState[headIndex[1]][headIndex[0]] = currentState[headIndex[1] - 1][headIndex[0]];
+        currentState[headIndex[1] - 1][headIndex[0]] = 0;
+        headIndex[1]--;
+        break;
+    case 'd':
+        currentState[headIndex[1]][headIndex[0]] = currentState[headIndex[1]][headIndex[0] - 1];
+        currentState[headIndex[1]][headIndex[0] - 1] = 0;
+        headIndex[0]--;
+        break;
+    case 'l':
+        currentState[headIndex[1]][headIndex[0]] = currentState[headIndex[1] + 1][headIndex[0]];
+        currentState[headIndex[1] + 1][headIndex[0]] = 0;
+        headIndex[1]++;
+        break;
     }
     game();
 }
@@ -94,7 +131,67 @@ void game(){
     printf("%d %d\n", headIndex[0], headIndex[1]);
 
     // handle the output
-    //todo: display the game array: currentState
+    // main game output
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 201, 205, 205, 205, 203, 205, 205, 205, 203, 205, 205, 205, 203, 205, 205, 205, 187);
+    for(int j = 0; j < 4; j++){
+        if(j != 0){
+            // Borders
+            if(headIndex[0] == j - 1){
+                switch(headIndex[1]){
+                    case 0:
+                        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 24, 205, 206, 205, 205, 205, 206, 205, 205, 205, 206, 205, 205, 205, 185);
+                        break;
+                    case 1:
+                        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 206, 205, 24, 205, 206, 205, 205, 205, 206, 205, 205, 205, 185);
+                        break;
+                    case 2:
+                        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 206, 205, 205, 205, 206, 205, 24, 205, 206, 205, 205, 205, 185);
+                        break;
+                    case 3:
+                        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 206, 205, 205, 205, 206, 205, 205, 205, 206, 205, 24, 205, 185);
+                        break;
+                }
+            }
+            else if(headIndex[0] == j){
+                switch(headIndex[1]){
+                    case 0:
+                        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 25, 205, 206, 205, 205, 205, 206, 205, 205, 205, 206, 205, 205, 205, 185);
+                        break;
+                    case 1:
+                        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 206, 205, 25, 205, 206, 205, 205, 205, 206, 205, 205, 205, 185);
+                        break;
+                    case 2:
+                        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 206, 205, 205, 205, 206, 205, 25, 205, 206, 205, 205, 205, 185);
+                        break;
+                    case 3:
+                        printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 206, 205, 205, 205, 206, 205, 205, 205, 206, 205, 25, 205, 185);
+                        break;
+                }
+            }
+            else{
+                printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 204, 205, 205, 205, 206, 205, 205, 205, 206, 205, 205, 205, 206, 205, 205, 205, 185);
+            }
+        }
+        // Numbers
+        printf("%c   %c   %c   %c   %c\n", 186, 186, 186, 186, 186);
+        for(int i = 0; i < 4; i++){
+            if(currentState[i][j] == 0 && i != 0){
+                printf(">   ");
+            }
+            else if(currentState[i - 1][j] == 0 && i != 0){
+                printf("< %2d", currentState[i][j]);
+            }
+            else if(currentState[i][j] == 0){
+                printf("%c   ", 186);
+            }
+            else{
+                printf("%c %2d", 186, currentState[i][j]);
+            }
+        }
+        printf("%c\n", 186);
+        printf("%c   %c   %c   %c   %c\n", 186, 186, 186, 186, 186);
+    }
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n", 200, 205, 205, 205, 202, 205, 205, 205, 202, 205, 205, 205, 202, 205, 205, 205, 188);
 
     //menu
     if(headIndex[0] != 3){
@@ -126,25 +223,25 @@ void game(){
             case 'w':
                 if(headIndex[0] != 3){
                     // UP
-                    headIndex[0]++;
+                    makeMove('u');
                 }
                 break;
             case 'd':
                 if(headIndex[1] != 0){
                     // RIGHT
-                    headIndex[1]--;
+                    makeMove('r');
                 }
                 break;
             case 's':
                 if(headIndex[0] != 0){
                     // DOWN
-                    headIndex[0]--;
+                    makeMove('d');
                 }
                 break;
             case 'a':
                 if(headIndex[1] != 3){
                     // LEFT
-                    headIndex[1]++;
+                    makeMove('l');
                 }
                 break;
             case 'm':
